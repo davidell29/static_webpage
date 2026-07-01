b52a3ace2554c3d064d8aee5054bc11e31159c8c
davidell29 <gabrielldavid65@gmail.com>
2026-07-01 20:53:50 +0300

    done with the split func

--- src/split_img_links.py
+++ src/split_img_links.py
@@ -7,5 +7,44 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
+            continue
+        
+        images = extract_markdown_images(node.text)
+        remaining_text = node.text
+        if not images:
+            new_nodes.append(node)
+        else:
+            for image_alt, image_link in images:
+                sections = remaining_text.split(f"![{image_alt}]({image_link})", 1)
+                if sections[0]:
+                    new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+                remaining_text = sections[1]
+                
+            if remaining_text:
+                new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+    return new_nodes
+
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        links = extract_markdown_links(node.text)
+        remaining_text = node.text
+        if not links:
+            new_nodes.append(node)
+        else:
+            for link_text, link_url in links:
+                sections = remaining_text.split(f"[{link_text}]({link_url})", 1)
+                if sections[0]:
+                    new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+                remaining_text = sections[1]
+
+            if remaining_text:
+                new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+    return new_nodes
---
+++ src/text_to_textnodes.py
@@ -0,0 +1,11 @@
+from split_img_links import split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType
+from split_dem import split_nodes_delimiter
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
